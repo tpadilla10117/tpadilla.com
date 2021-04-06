c56f6b9d946dc4637befdae8377a6ac65b490943
Trinidad Padilla <tpadilla101@live.com>
2021-04-06 11:47:26 -0700

    need to make contact.js file to split components

--- my-app/src/components/feature/EmailForm.js
+++ my-app/src/components/feature/EmailForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import '../../assets/css/EmailForm.css'
-
-  import { SiGithub, SiLinkedin, SiInstagram, SiFacebook  } from "react-icons/si";
+import Skills from '../layout/Skills';
+import { SiGithub, SiLinkedin, SiInstagram, SiFacebook  } from "react-icons/si";
 
 
 
@@ -9,50 +9,53 @@ import '../../assets/css/EmailForm.css'
 const EmailFormComponent = (props) => {
 
     return <> 
-        <div id="form-wrapper">
-            
-            <form className="email-form">
-                <div className="intro-text">
-                    <h5>Want to Get in Touch? <br/> Send me an Email!</h5>
-                </div>
-                <div className="name-input">
-                    <div id="input-one">
-                        <label for="first-name" className="first-name" required>First Name: </label>
-                        <input type="text" placeholder="Sasuke"></input>
+        <div id="component-wrapper">
+            <div className="skills-component"> <Skills /> </div>
+            <div id="form-wrapper">
+                
+                <form className="email-form">
+                    <div className="intro-text">
+                        <h5>Want to Get in Touch? <br/> Send me an Email!</h5>
+                    </div>
+                    <div className="name-input">
+                        <div id="input-one">
+                            <label for="first-name" className="first-name" required>First Name: </label>
+                            <input type="text" placeholder="Sasuke"></input>
+                        </div>
+
+                        <div id="input-two">
+                            <label for="last-name" className="last-name" required>Last Name: </label>
+                            <input type="text" placeholder="Uchiha"></input>
+                        </div>
+                    </div>
+                    <br/>
+                    <div className="email-input">
+                        <label for="email" id="email" className="input-two">Email: </label>
+                        <input type="email" placeholder="avenger@uchiha.com" name="email"></input>
+                    </div>
+                    <div className="btn-box">
+                        <p>By sending a message, you agree to not spam</p>
+                        <textarea className="message-text" placeholder="What would you like to ask me?" />
+                        <button type="submit" value="Submit">Submit</button>
+                    </div>
+                    <br/>
+
+                    {/* <textarea placeholder="What would you like to ask me?" /> */}
+
+                    <br/>
+                    <div className="other-content">
+                        <p>Check me out on Social Media!</p>
+                        {/* 4/4: Look at how i sized the github icon */}
+                        <div className="links">
+                                <a href="#" className="github">{<SiGithub /* size={70} *//>}</a>
+                                <a href="#" className="facebook">{<SiFacebook />}</a>
+                                <a href="#" className="instagram">{<SiInstagram />}</a>
+                                <a href="https://www.linkedin.com/in/t-p-101/" className="linkedin">{<SiLinkedin />}</a>
+                        </div>
                     </div>
 
-                    <div id="input-two">
-                        <label for="last-name" className="last-name" required>Last Name: </label>
-                        <input type="text" placeholder="Uchiha"></input>
-                    </div>
-                </div>
-                <br/>
-                <div className="email-input">
-                    <label for="email" id="email" className="input-two">Email: </label>
-                    <input type="email" placeholder="avenger@uchiha.com" name="email"></input>
-                </div>
-                <div className="btn-box">
-                    <p>By sending a message, you agree to not spam</p>
-                    <textarea className="message-text" placeholder="What would you like to ask me?" />
-                    <button type="submit" value="Submit">Submit</button>
-                </div>
-                <br/>
-
-                {/* <textarea placeholder="What would you like to ask me?" /> */}
-
-                <br/>
-                <div className="other-content">
-                    <p>Check me out on Social Media!</p>
-                    {/* 4/4: Look at how i sized the github icon */}
-                    <div className="links">
-                            <a href="#" className="github">{<SiGithub /* size={70} *//>}</a>
-                            <a href="#" className="facebook">{<SiFacebook />}</a>
-                            <a href="#" className="instagram">{<SiInstagram />}</a>
-                            <a href="https://www.linkedin.com/in/t-p-101/" className="linkedin">{<SiLinkedin />}</a>
-                    </div>
-                </div>
-
-            </form>
+                </form>
+            </div>
         </div>
     
     </>
