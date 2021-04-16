994a1dc839370af40e21906b2c37dd26255249ec
Trinidad Padilla <tpadilla101@live.com>
2021-04-15 17:43:04 -0700

    chose fonts

--- my-app/src/components/feature/AboutMe.js
+++ my-app/src/components/feature/AboutMe.js
@@ -16,7 +16,7 @@ const AboutMeComponent = (props) => {
                     <img src={Image1} className="about-picture" alt="Trin Padilla" loading="lazy" id="left-image"></img>
                 </div>
                 <div className="sideBio">
-                    <h1>Heya!</h1><br/>
+                    <h1 className="about-heading">Slow & Steady Wins The Race...</h1><br/><br/>
                     <p>I'm a Jack-of-All-Trades: "Master of None though oftentimes better than a Master of One" type who's dediciated his life to learning and specializing in a handful of trades.  I find myself fascinated with both the creative arts and problem solving, which began when I picked up my first guitar at 16-years-old.  When I'm not melting faces with a rock solo or grooving out to a funk jam, you can find me "turtling" (yes - it's a thing!) it up behind a code editor or DAW.  I have a knack for rapid ideation in both music and design, especially in audio production and on digital mediums.  Like music and audio, users experience web-applications every day, and one of my many responsibilities is to ensure those experiences are profound, memorable, and inutitive (to name a few).  Whether behind a guitar, ProTools, pen & paper, or a code-editor, it's my duty to convey these experiences.   </p>
                     <br/><hr/><br/><br/>
                     <div className="links-about">
