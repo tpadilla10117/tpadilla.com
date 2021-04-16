66998c9e73f6879e02c94ed1fc725459d9bb8cc8
Trinidad Padilla <tpadilla101@live.com>
2021-04-15 17:25:02 -0700

    styled socials in about me

--- my-app/src/components/feature/AboutMe.js
+++ my-app/src/components/feature/AboutMe.js
@@ -5,6 +5,7 @@ import Image1 from "../../../src/assets/images/trin.jpg";
 import { IoLogoJavascript, IoLogoNodejs, IoLogoCss3 } from "react-icons/io";
 import { SiPostgresql, SiReact  } from "react-icons/si";
 import { FaStripe } from "react-icons/fa";
+import { SiGithub, SiLinkedin, SiInstagram, SiFacebook  } from "react-icons/si";
 
 const AboutMeComponent = (props) => {
 
@@ -17,15 +18,21 @@ const AboutMeComponent = (props) => {
                 <div className="sideBio">
                     <h1>Heya!</h1><br/>
                     <p>I'm a Jack-of-All-Trades: "Master of None though oftentimes better than a Master of One" type who's dediciated his life to learning and specializing in a handful of trades.  I find myself fascinated with both the creative arts and problem solving, which began when I picked up my first guitar at 16-years-old.  When I'm not melting faces with a rock solo or grooving out to a funk jam, you can find me "turtling" (yes - it's a thing!) it up behind a code editor or DAW.  I have a knack for rapid ideation in both music and design, especially in audio production and on digital mediums.  Like music and audio, users experience web-applications every day, and one of my many responsibilities is to ensure those experiences are profound, memorable, and inutitive (to name a few).  Whether behind a guitar, ProTools, pen & paper, or a code-editor, it's my duty to convey these experiences.   </p>
-                    <br/><hr/><br/>
-                    <ul className="stackIcons">
+                    <br/><hr/><br/><br/>
+                    <div className="links-about">
+                                <a href="#" className="github">{<SiGithub size={45}/>}</a>
+                                <a href="#" className="facebook">{<SiFacebook size={45}/>}</a>
+                                <a href="#" className="instagram">{<SiInstagram size={45}/>}</a>
+                                <a href="https://www.linkedin.com/in/t-p-101/" className="linkedin">{<SiLinkedin size={45}/>}</a>
+                    </div>
+                    {/* <ul className="stackIcons">
                         <li className="icons">{<IoLogoJavascript />}</li>
                         <li className="icons">{<SiPostgresql/>}</li>
                         <li className="icons">{<IoLogoNodejs/>}</li>
                         <li className="icons"> {<FaStripe/>}</li>
                         <li className="icons">{<IoLogoCss3/>}</li>
                         <li className="icons"> {<SiReact/>}</li>
-                    </ul>
+                    </ul> */}
                 </div>
             </div>
            
