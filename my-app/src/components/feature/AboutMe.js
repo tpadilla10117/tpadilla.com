d72d6fb421a15fe76bdb0afe68c3c233194b47f2
Trinidad Padilla <tpadilla101@live.com>
2021-04-19 20:44:36 -0700

    fixed about

--- my-app/src/components/feature/AboutMe.js
+++ my-app/src/components/feature/AboutMe.js
@@ -23,7 +23,7 @@ const AboutMeComponent = (props) => {
                         <div className="img-content">
                             <div>
                                 {/* <h2>Image Title</h2> */}
-                                <p>Me in my natural form</p>
+                                <p>"Hello There"</p>
                             </div>
                         </div>
                     </div>
@@ -31,7 +31,8 @@ const AboutMeComponent = (props) => {
 
 
                 <div className="sideBio">
-                    <h1 className="about-heading">L.O.A.D.I.N.G.</h1><br/><br/>
+                    <h1 className="about-heading">L.O.A.D.I.N.G.</h1><br/>
+                    <h3>Lead On Aspiring Dreamer In Neverending Glory</h3><br/>
                     <p>I'm a Jack-of-All-Trades: "Master of None though oftentimes better than a Master of One" type who's dediciated his life to learning and specializing in a handful of trades.  I find myself fascinated with both the creative arts and problem solving, which began when I picked up my first guitar at 16-years-old.  When I'm not melting faces with a rock solo or grooving out to a funk jam, you can find me "turtling" (yes - it's a thing!) it up behind a code editor or DAW.  I have a knack for rapid ideation in both music and design, especially in audio production and on digital mediums.  Like music and audio, users experience web-applications every day, and one of my many responsibilities is to ensure those experiences are profound, memorable, and inutitive (to name a few).  Whether behind a guitar, ProTools, pen & paper, or a code-editor, it's my duty to convey these experiences.   </p>
                     <br/><hr/><br/><br/>
                     <div className="links-about">
