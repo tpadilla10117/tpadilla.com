6870ada774988c2f14cb44dce2d908d840f6edd4
Trinidad Padilla <tpadilla101@live.com>
2021-05-20 13:54:58 -0700

    got some basic transitions done, but need to fix so that they dont persist when loading another route

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -2,29 +2,42 @@
 
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { BrowserRouter as Router, Route, Switch, Redirect, Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Header, EmailFormComponent, AboutMeComponent, Portfolio, ContactComponent, Landing, NavbarComponent } from './index';
 
 import { Flipped } from '../utils/index.js';
 
 function App() {
-
+  const location = useLocation();
   const [ flip, setFlip ] = useState(Flipped[0]);
   const [ active, isActive ] = useState("false");
   /* console.log(Flipped[0]) */ //returns the array at its index
   console.log("The flip from App component:", flip)
   return (
     
-    <Router>
+            <div>
                 <Header />
-                <Switch>
-                    <Route path="/portfolio" exact component={Portfolio} />
-                    <Route path="/contact" exact component={ContactComponent} />
-                    <Route path="/about" exact component={() => <AboutMeComponent active={active} isActive={isActive}flip={flip} setFlip={setFlip}/>} />
-                    <Route path="/landing" exact component={Landing} />
-                </Switch>
-            
-        </Router>
+                <TransitionGroup>
+                  <CSSTransition
+                    timeout={350}
+                    classNames='fade'
+                    /* Applies Transition */
+                    key={location.key}>
+                      
+                    <Switch location={location}>
+                    
+                        <Route exact path="/portfolio" exact component={Portfolio} />
+                        <Route exact path="/contact" exact component={ContactComponent} />
+                        <Route exact path="/about" exact component={() => <AboutMeComponent active={active} isActive={isActive}flip={flip} setFlip={setFlip}/>} />
+                        <Route exact path="/landing" exact component={Landing} />
+                      
+                    </Switch>
+                  </CSSTransition>
+                </TransitionGroup>
+              </div>
+        
   );
 }
 
