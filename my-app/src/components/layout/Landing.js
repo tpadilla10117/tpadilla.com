9ed8db48db7eb6a58c56e8ed3e993bb5397ebc1a
Trinidad Padilla <tpadilla101@live.com>
2021-05-20 15:29:56 -0700

    linked projects and some styling.  Still need to fix transitions on card, and a navigation

--- my-app/src/components/layout/Landing.js
+++ my-app/src/components/layout/Landing.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState, Component } from 'react';
 
 import '../../assets/css/Landing.css';
-import Image3 from "../../../src/assets/images/crwn_clothing_landing.png";
-import Image4 from "../../../src/assets/images/codalorians.png";
+import CrwnClothing from "../../../src/assets/images/crwn_clothing_landing.png";
+import Codalorians from "../../../src/assets/images/codalorians.png";
 import PixelDraw from "../../../src/assets/images/pixel_draw.png";
-import Image6 from "../../../src/assets/images/postbook.png";
-import Image7 from "../../../src/assets/images/robo_search.png";
+import Postbook from "../../../src/assets/images/postbook.png";
+import RoboSearch from "../../../src/assets/images/robo_search.png";
 /* import ProjectCards from '../feature/ProjectCards';
 import { picBox } from '../../utils'; */
 
@@ -20,26 +20,26 @@ const Landing = (props) => {
         <div className="projCard-container">
             <div className="projCard-box">
                 <div className="img-wrapper">
-                    <img src={Image3}/>
+                    <img src={CrwnClothing} alt="A clickable card of an application called `Crwn Clothing`"/>
                 </div>
 
                 <div className="details-box">
                 <div className="content-box">
                     {/* <h2>This is the Card Title</h2> */}
-                    <p>This is the actual content on the card</p>
+                    <p>An e-commerce for Stylish, Trendy Clothing</p>
                 </div>
                 </div>
 
             </div>
             <div className="projCard-box">
                 <div className="img-wrapper">
-                    <img src={Image4}/>
+                    <img src={Codalorians} alt="A clickable card of an application called `Codalorians`"/>
                 </div>
 
                 <div className="details-box">
                 <div className="content-box">
                     {/* <h2>This is the Card Title</h2> */}
-                    <p>This is the actual content on the card</p>
+                    <p>An e-commerce for Musical Instruments</p>
                 </div>
                 </div>
             </div>
@@ -47,42 +47,47 @@ const Landing = (props) => {
 
             <div className="projCard-box">
                 <div className="img-wrapper">
-                    <img src={PixelDraw}/>
+                    <img src={PixelDraw} alt="A clickable card of an application called `Pixel Draw`"/>
                 </div>
 
                 <div className="details-box">
                 <div className="content-box">
                     {/* <h2>This is the Card Title</h2> */}
-                    <p>This is the actual content on the card</p>
+                    <p>A Simple, Grid-Based, Pixel-Art Creator</p>
                 </div>
                 </div>
             </div>
             </a>
             
-            <div className="projCard-box">
-                <div className="img-wrapper">
-                    <img src={Image6}/>
-                </div>
+            <a href="https://laughing-austin-fe12b7.netlify.app/#">
+                <div className="projCard-box">
+                    <div className="img-wrapper">
+                        <img src={Postbook} alt="A clickable card of an application called `PostBook`"/>
+                    </div>
 
-                <div className="details-box">
-                <div className="content-box">
-                    {/* <h2>This is the Card Title</h2> */}
-                    <p>This is the actual content on the card</p>
+                    <div className="details-box">
+                    <div className="content-box">
+                        {/* <h2>This is the Card Title</h2> */} 
+                        <p>A Craigslist Inspired App Where Users Can Post Items For Sale</p>
+                    </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-            <div className="projCard-box">
-                <div className="img-wrapper">
-                    <img src={Image7}/>
-                </div>
+            </a>
 
-                <div className="details-box">
-                <div className="content-box">
-                    {/* <h2>This is the Card Title</h2> */}
-                    <p>This is the actual content on the card</p>
+            <a href="https://tpadilla10117.github.io/robo_search/">
+                <div className="projCard-box">
+                    <div className="img-wrapper">
+                        <img src={RoboSearch} alt="A clickable card of an application called `Robo Search`"/>
+                    </div>
+                    
+                    <div className="details-box">
+                    <div className="content-box">
+                        {/* <h2>This is the Card Title</h2> */}
+                        <p>A Way To Search For Product Cards</p>
+                    </div>
+                    </div>
                 </div>
-                </div>
-            </div>
+            </a>
             {/* <div className="projCard-box">
                 <div className="img-wrapper">
                     <img src={Image3}/>
