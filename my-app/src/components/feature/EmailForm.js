942ea961e9ffa53a1f17ffd19e4dfc9183bc4e73
Trinidad Padilla <tpadilla101@live.com>
2021-05-24 21:04:56 -0700

    set up my email form.  Tested and works

--- my-app/src/components/feature/EmailForm.js
+++ my-app/src/components/feature/EmailForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../../assets/css/EmailForm.css'
-import Skills from '../layout/Skills';
 import { SiGithub, SiLinkedin, SiInstagram, SiFacebook  } from "react-icons/si";
 
 
@@ -10,37 +9,41 @@ const EmailFormComponent = (props) => {
 
     return <> 
         <div id="component-wrapper">
-            {/* <div className="skills-component"> <Skills /> </div> */}
             <div id="form-wrapper">
                 
-                <form className="email-form">
+                <form className="email-form" action="https://formsubmit.co/socratesclown@gmail.com" method="POST">
                     <div className="intro-text">
                         <h5>Want to Get in Touch? <br/> Send me an Email!</h5>
                     </div>
                     <div className="name-input">
                         <div id="input-one">
                             <label for="first-name" className="first-name" required>First Name: </label>
-                            <input type="text" placeholder="Sasuke"></input>
+                            <input type="text" placeholder="Sasuke" name="email" required></input>
                         </div>
 
                         <div id="input-two">
                             <label for="last-name" className="last-name" required>Last Name: </label>
-                            <input type="text" placeholder="Uchiha"></input>
+                            <input type="text" placeholder="Uchiha" name="name" required></input>
                         </div>
                     </div>
                     <br/>
+                    
                     <div className="email-input">
                         <label for="email" id="email" className="input-two">Email: </label>
                         <input type="email" placeholder="avenger@uchiha.com" name="email"></input>
                     </div>
+                    <br/>
+                    <div>
+                        <label for="_subject" id="_subject" className="_subject">Subject </label>
+                        <input type="text" name="_subject" /* value="New Email From Personal Site!" *//>
+                    </div>
                     <div className="btn-box">
                         <p>By sending a message, you agree to not spam</p>
-                        <textarea className="message-text" placeholder="What would you like to ask me?" />
+                        <textarea className="message-text" placeholder="What would you like to ask me?" type="text" name="name"/>
                         <button type="submit" value="Submit">Submit</button>
                     </div>
+                    {/* <input type="hidden" name="_next" value="https://yourdomain.co/thanks.html"></input> */}
                     <br/>
-
-                    {/* <textarea placeholder="What would you like to ask me?" /> */}
 
                     <br/>
                     <div className="other-content">
