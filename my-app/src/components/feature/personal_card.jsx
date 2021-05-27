3d918ae53e09d39ebc41ad60cf26f6399ae97cc8
Trinidad Padilla <tpadilla101@live.com>
2021-05-26 21:26:34 -0700

    edited the text of my project cards, changed up the primary and secondary colors, readjusted the landing page.  Still need to make mobile responsive nav and components

--- my-app/src/components/feature/personal_card.jsx
+++ my-app/src/components/feature/personal_card.jsx
@@ -1,8 +1,11 @@
 import React  from 'react';
 import Hike from "../../../src/assets/images/hike.jpeg";
 import GuitarSelfie from "../../../src/assets/images/GuitarSelfie.jpeg";
-import mtdiablo from "../../../src/assets/images/mtdiablo.jpeg";
 import SanDiego from "../../../src/assets/images/SanDiego.jpeg";
+import FortBragg1 from "../../../src/assets/images/FortBragg1.jpeg";
+/* import FortBragg2 from "../../../src/assets/images/FortBragg2.jpeg";
+import FortBragg4 from "../../../src/assets/images/FortBragg4.jpeg";
+import BeachPano from "../../../src/assets/images/BeachPano.jpeg"; */
 
 import './personal_card.css';
 
@@ -25,19 +28,21 @@ const PersonalCard = (props) => {
     
 
     const cardInfo = [
-        { id: 1, name: "card card-1", image: `${Hike}`, frontHeader: "Fitness", backHeader2: '"Health is Wealth"', backHeader3: "Peace of Mind", text: "My greatest competitor is myself.  I don't compete with anyone.  Instead, I strive to push myself and constantly improve" },
-        { id: 2, name: "card card-2",image: `${GuitarSelfie}`, frontHeader: "Guitarist", backHeader2: '"In the Deepest Ocean..."', backHeader3: "Music", text: "In my spare time I like to jam" },
-        { id: 3, name: "card card-3",image: `${SanDiego}`, frontHeader: "Adventurer", backHeader2:'"One Does Not Simply Walk into Mordor..."' , backHeader3: "Traversing the Earth", text: "As a suburb kid, I've dreamed of leaving my confounds and..." }
+        { id: 1, name: "card card-1", image: `${Hike}`, frontHeader: "Health", backHeader2: '"Health is Wealth"', backHeader3: "Peace of Mind", text: "I strive to not compete with anyone but myself while pushing forward to improve in each of my endeavors: A healthy mind & body, functional / nourshing relationships, and impactful work.", BackImage: `${FortBragg1}` },
+        { id: 2, name: "card card-2",image: `${GuitarSelfie}`, frontHeader: "Guitarist", backHeader2: '"In the Deepest Ocean..."', backHeader3: "Music", text: "Performing and professional audio are two passions I cannot live without.  Whether it's rocking out to my favorite groove or indie jam, or analyzing the complex soundscapes of a recording, music is part of my identity. ", BackImage: `${FortBragg1}` },
+        { id: 3, name: "card card-3",image: `${SanDiego}`, frontHeader: "Adventurer", backHeader2:'"One Does Not Simply Walk into Mordor..."' , backHeader3: "Traversing the Earth", text: "As a California native, I've dreamed of expanding my worldview beyond my suburban confounds.  Life is meant to be lived - there are countless people to meet, places to explore, and experiences to be had.  Find me at a beach, in a new city, or even another country.", BackImage: `${FortBragg1}` }
     ]
 
 
     const renderCard = (card, index) => {
         return(
             <div className={card.name} key={"card-" + index}>
-                <div className={toggleActiveStyles(index)} onClick={() => { toggleActive(index)}}/* {active ? "card__inner" : `card__inner is-flipped active`} onClick={handleClassToggle} */>
+                <div className={toggleActiveStyles(index)} onClick={() => { toggleActive(index)}}>
                     <div className="card__face card__face--front">
-                    <h2>{card.frontHeader}</h2>
-                    {/* <img className="" src={card.image} alt=""/> */}
+                        <h2>{card.frontHeader}</h2>
+                        <div className="img__container">
+                            <img className="front__image" src={card.BackImage} alt=""/>
+                        </div>
                     </div>
 
                     <div className="card__face card__face--back">
