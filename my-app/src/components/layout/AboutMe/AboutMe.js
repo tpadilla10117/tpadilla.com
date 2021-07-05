83300372911f0f681cb31fd09ce83b8522635ce0
Trinidad Padilla <tpadilla101@live.com>
2021-07-05 15:51:42 -0700

    fixed some formatting issues in the about section

--- my-app/src/components/layout/AboutMe/AboutMe.js
+++ my-app/src/components/layout/AboutMe/AboutMe.js
@@ -22,7 +22,7 @@ const AboutMeComponent = (props) => {
                         </div>
                         <div className="img-content">
                             <div>
-                                <p>Me, currently being a dweeb</p>
+                                <p>Hey There! I'm Trin!</p>
                             </div>
                         </div>
                     </div>
@@ -32,8 +32,8 @@ const AboutMeComponent = (props) => {
                 <div className="sideBio">
                     <h1 className="about-heading">{/* L.O.A.D.I.N.G. */}Hello There!</h1><br/>
                     <h3>{/* Lead On Aspiring Dreamer In Neverending Glory */}Full Stack UX Engineer in Training</h3><br/>
-                    <p>I'm a Jack-of-All-Trades: "Master of None though oftentimes better than a Master of One" type who's dediciated his life to both learning and specializing in the following trades: UX Design, Full Stack Web Development, and Pro Audio.  I find myself fascinated with both the creative arts and problem solving, which began when I picked up my first guitar at 16-years-old and continues with each line of code I write.  When I'm not grooving out to a funk jam, you can find me "turtling", (slowly perfecting my craft), it up behind a code editor or DAW.  I have a knack for rapid ideation and creativity in both music and UX design, especially in audio production and frontend development.  Like music, users experience web-applications every day, and one of my many responsibilities is to ensure those user experiences are profound, memorable, and inutitive (to name a few).  Whether behind a guitar, ProTools, pen & paper, Figma, or a code-editor, it's my duty to convey these experiences.   </p>
-                    <br/><hr/>
+                    <p>I'm a Jack-of-All-Trades: "Master of None though oftentimes better than a Master of One" type who's dediciated his life to both learning and specializing in the following trades: UX Design, Full Stack Web-Development, and Pro Audio.  I find myself fascinated with both the creative arts and problem solving.  When I'm not grooving out to a funk jam, you can find me "turtling" (slowly perfecting my craft), it up behind a code editor or DAW.  I have a knack for rapid ideation and creativity in each area of focus.  Like music, users experience web-applications every day, and one of my many responsibilities is to ensure those user experiences are profound, memorable, and inutitive (to name a few).  Whether behind a guitar, ProTools, pen & paper, Figma, or VS Code, it's my duty to convey these experiences in a manner that users will enjoy!  Get to know a bit more about me and my work below!   </p>
+                    
                     <div className="links-about">
                                 <a href="https://github.com/tpadilla10117" className="github">{<SiGithub size={45}/>}</a>
                                 <a href="#" className="facebook">{<SiFacebook size={45}/>}</a>
